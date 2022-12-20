b79a91c5c8c9e5fabf6d379c04dea57e1e2a4a78
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-20 16:26:06 +0300

    commit tree 72a06f9dce43fdcbbf5064bf1cb0da4976a8df42

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+let employee = {
+    name:"",
+    streetAdress:""
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return{
+        ...employee,
+        [key]:value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress ="12 Broadway";
+    return employee
+    
+}
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newEmployee = Object.assign({}, employee);
+    
+//     delete newEmployee[key];
+    
+//     return newEmployee;
+//   }
+
+function deleteFromEmployeeByKey(employee, key) {
+    const deletedEmployees = {...employee}
+    delete deletedEmployees[key];
+    return deletedEmployees
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+    return employee
+  }
